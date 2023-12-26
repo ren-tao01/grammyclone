937b404787dfb6ebddbc45f37235ff71e6581ab0
Ren Tao Wan <rtwan1@sheffield.ac.uk>
2023-12-26 01:42:19 +0000

    added explore logo

--- src/assets/constants.jsx
+++ src/assets/constants.jsx
@@ -17,6 +17,56 @@ export const InstagramLogo = () => (
   </svg>
 );
 
+export const ExploreLogo = () => (
+  <svg
+    aria-label="Explore"
+    color="rgb(245, 245, 245)"
+    fill="rgb(245, 245, 245)"
+    height="24"
+    role="img"
+    viewBox="0 0 24 24"
+    width="24"
+  >
+    <title>Explore</title>
+
+    <path
+      fill="none"
+      d="M13.941 13.953 7.581 16.424 10.06 10.056 16.42 7.585 13.941 13.953Z"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+    ></path>
+    <path
+      fillRule="evenodd"
+      d="M10.06 10.056 13.949 13.945 7.581 16.424 10.06 10.056Z"
+    ></path>
+
+    {/* <polygon
+        fill="none"
+        points="13.941 13.953 7.581 16.424 10.06 10.056 16.42 7.585 13.941 13.953"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+      ></polygon>
+    <polygon
+      fillRule="evenodd"
+      points="10.06 10.056 13.949 13.945 7.581 16.424 10.06 10.056"
+    ></polygon>*/}
+    <circle
+      cx="12.001"
+      cy="12.005"
+      fill="none"
+      r="10.5"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+    ></circle> 
+  </svg>
+);
+
 export const InstagramMobileLogo = () => (
   <svg
     aria-label="Instagram"
