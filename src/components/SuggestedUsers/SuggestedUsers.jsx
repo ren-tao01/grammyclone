f86381d45c01d7a393bd3d9698db96e5dbc31e5a
Ren Tao Wan <rtwan1@sheffield.ac.uk>
2023-12-26 20:24:13 +0000

    Remove red border

--- src/components/SuggestedUsers/SuggestedUsers.jsx
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 
 export const SuggestedUsers = () => {
   return (
-    <VStack py={8} px={4} gap={4}  border={"2px solid red"}>
+    <VStack py={8} px={4} gap={4}>
       <SuggestedHeader />
 
       <Flex alignItems={"center"} justifyContent={"space-between"} w={"full"}>
@@ -23,13 +23,25 @@ export const SuggestedUsers = () => {
         </Text>
       </Flex>
 
-      <SuggestedUser name="Dan Steven" followers={3891} avatar="https://bit.ly/dan-abramov" />
-      <SuggestedUser name="Dan Steven" followers={3891} avatar="https://bit.ly/dan-abramov" />
-      <SuggestedUser name="Dan Steven" followers={3891} avatar="https://bit.ly/dan-abramov" />
+      <SuggestedUser
+        name="Dan Steven"
+        followers={3891}
+        avatar="https://bit.ly/dan-abramov"
+      />
+      <SuggestedUser
+        name="Dan Steven"
+        followers={3891}
+        avatar="https://bit.ly/dan-abramov"
+      />
+      <SuggestedUser
+        name="Dan Steven"
+        followers={3891}
+        avatar="https://bit.ly/dan-abramov"
+      />
 
       {/* target = _blank; is for opening new tab */}
       <Box alignSelf={"start"} fontSize={12} color={"gray.500"} mt={5}>
-        © 2023 Built by {" "}
+        © 2023 Built by{" "}
         <Link
           to={"https://www.linkedin.com/in/1rentaowan1/"}
           target="_blank"
